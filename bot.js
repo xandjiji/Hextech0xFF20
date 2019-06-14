c78e51112a53d0ecda834fe2f406805cb549888e
xand <xandjiji@gmail.com>
2019-06-14 20:06:36 -0300

    renaming files

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-
+var Jimp        = require('jimp');
 var fs          = require('fs');
 
 var keys        = require('./keys.env');
@@ -10,6 +10,58 @@ asd = utils.sha256(asd);
 
 let teste = utils.makeScoreboard(asd);
 
-console.log(teste);
+var jimps = [];
+loadAssets();
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// loading assets
+function loadAssets() {
+
+    logging.loadingAssetsMsg();
+
+    // loading template
+    jimps.push(Jimp.read('assets/template.png'));
+
+    // loading champions
+    for(let i = 1; i < 253; i++) {
+        jimps.push(Jimp.read('assets/champions/pokemon' + (i-1) + '.png'));
+    }
+
+    // carregando insigneas (kanto)
+    for(var i = 253; i < 261; i++) {
+        jimps.push(Jimp.read('assets/badges/kanto/kanto' + (i-253) + '.png'));
+    }
+
+    // carregando insigneas (johto)
+    for(var i = 261; i < 269; i++) {
+        jimps.push(Jimp.read('assets/badges/johto/johto' + (i-253) + '.png'));
+    }
+
+    // carregando treinadores
+    for(var i = 269; i < 376; i++) {
+        jimps.push(Jimp.read('assets/trainers/trainer' + (i-269) + '.png'));
+    }
+
+    // salvando template
+    jimps.push(Jimp.read('assets/card.png'));
+}
